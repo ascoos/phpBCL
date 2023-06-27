4845509d812265b9cda0a0ac08aba2253cc3e087
Ascoos Development <ascoos@users.noreply.github.com>
2023-06-27 16:51:50 +0300

    Add file for similar functions

---
+++ src/compat/compat_similar.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ *
+ *   __ _  ___  ___ ___   ___   ___     ____ _ __ ___   ___
+ *  / _` |/  / / __/ _ \ / _ \ /  /    / __/| '_ ` _ \ /  /
+ * | (_| |\  \| (_| (_) | (_) |\  \   | (__ | | | | | |\  \
+ *  \__,_|/__/ \___\___/ \___/ /__/    \___\|_| |_| |_|/__/
+ * 
+ * 
+ ********************************************************************
+ * @ Project ASCOOS                                                 *
+ * @ Copyright (C) 2007 - 2023 AlexSoft Software.                   *
+ * @ Creator: Drogidis Christos                                     *
+ * @ ASCOOS CMS Site: www.ascoos.com                                *
+ * @ Creator Site: www.alexsoft.gr                                  *
+ * @ email: support@ascoos.com                                      *
+ * @ license site: http://docs.ascoos.com/lics/ascoos/AGL.html      *
+ * @ Copyrighted Commercial Software                                *
+ * @ Program ASCOOS CMS Manager                                     *
+ ********************************************************************
+ *
+ * @ Package           : ASCOOS CMS - phpBCL
+ * @ Subpackage        : Core Similar Compatibilities Manager
+ * @ ASCOOS Version    : Ten - 1.0.0
+ * @ File Name         : /phpBCL/compat/compat_similar.php
+ * @ File No.          : ?? - $release: 1.0 - $revision: 0 - $build 0
+ * @ Created Date      : 2023-06-26 07:00:00 UTC+3
+ * @ Updated Date      : 
+ * @ Author            : Drogidis Christos
+ * @ Author email      : webmaster@alexsoft.gr
+ * @ Author website    : www.alexsoft.gr
+ *
+ */
+
+
+// Run on ASCOOS CMS only. Marked as comment if you want run this script with other cms.
+defined ("ALEXSOFT_RUN_CMS") or die("Prohibition of Access.");
+
+
+
+
+
+
+?>
