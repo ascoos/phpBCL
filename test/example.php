5eb7d42114f5ab601a48ed00f8fd0b518c9c835f
Ascoos Development <ascoos@users.noreply.github.com>
2023-07-08 03:28:17 +0300

    Create example.php

---
+++ test/example.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *   __ _  ___  ___ ___   ___   ___     ____ _ __ ___   ___
+ *  / _` |/  / / __/ _ \ / _ \ /  /    / __/| '_ ` _ \ /  /
+ * | (_| |\  \| (_| (_) | (_) |\  \   | (__ | | | | | |\  \
+ *  \__,_|/__/ \___\___/ \___/ /__/    \___\|_| |_| |_|/__/
+ * 
+ * 
+ *************************************************************************************
+ * @ASCOOS-NAME        : ASCOOS CMS 24'                                              *
+ * @ASCOOS-VERSION     : 24.0.0                                                      *
+ * @ASCOOS-CATEGORY    : Kernel (Frontend and Administration Side)                   *
+ * @ASCOOS-CREATOR     : Drogidis Christos                                           *
+ * @ASCOOS-SITE        : www.ascoos.com                                              *
+ * @ASCOOS-LICENSE     : [Commercial] http://docs.ascoos.com/lics/ascoos/AGL-F.html  *
+ * @ASCOOS-COPYRIGHT   : Copyright (c) 2007 - 2023, AlexSoft Software.               *
+ *************************************************************************************
+ *
+ * @package            : ASCOOS CMS - phpBCL
+ * @subpackage         : Example file
+ * @source             : /phpBCL/test/example.php
+ * @version            : **** - $release: 1.0 - $revision: 1 - $build: ****
+ * @created            : 2023-07-07 07:00:00 UTC+3
+ * @updated            : 
+ * @author             : Drogidis Christos
+ * @authorSite         : www.alexsoft.gr
+ */
+
+define('ALEXSOFT_RUN_CMS', true);
+
+require_once("../src/coreCompatibilities.php");
+
+echo 'Hello!';
+
+ ?>
