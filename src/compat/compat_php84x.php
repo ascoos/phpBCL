22592dc78198c827c460ce8dda3d0697e2ed60af
Ascoos Development <ascoos@users.noreply.github.com>
2024-02-20 04:35:20 +0200

    Updated to 1.0.5

--- src/compat/compat_php84x.php
+++ src/compat/compat_php84x.php
@@ -21,7 +21,7 @@
  * @source             : /phpBCL/src/compat/compat_php84x.php
  * @version            : **** - $release: 1.0 - $revision: 1 - $build: ****
  * @created            : 2024-02-14 05:40:00 UTC+3
- * @updated            : 2024-02-16 05:40:00 UTC+3
+ * @updated            : 2024-02-20 03:00:00 UTC+3
  * @author             : Drogidis Christos
  * @authorSite         : www.alexsoft.gr
  */
@@ -31,9 +31,13 @@
 defined ("ALEXSOFT_RUN_CMS") or die("Prohibition of Access.");
 
 
+
+
 /**
  * If the function [ mb_ucfirst ] does not exist then we create it.
  * ++ 8.4.0 ---- https://wiki.php.net/rfc/mb_ucfirst
+ * 
+ * @since 1.0.5
  */
 if (!function_exists('mb_ucfirst'))
 {
@@ -45,18 +49,13 @@ if (!function_exists('mb_ucfirst'))
      */
     function mb_ucfirst($string, $encoding = null)
     {
-        // If $encoding is empty then we take the default internal encoding, otherwise we use the one given by the user.
-        $encoding = (is_null($encoding)) ? $encoding = mb_internal_encoding() : (string) $encoding;
-
         /**
          * 
          * Array with Strings Errors for this function.
          */
         $errors = array (
             'mb_ucfirst(): expects parameter 1 to be string, '.gettype($string).' given',
-            'mb_ucfirst(): Parameter 1 must not be empty',
-            'mb_ucfirst(): expects parameter 2 to be string, '.gettype($encoding).' given',          
-            'mb_ucfirst(): Unknown encoding "'.$encoding.'"'            
+            'mb_ucfirst(): Parameter 1 must not be empty',           
         );
 
         /******************
@@ -74,29 +73,8 @@ if (!function_exists('mb_ucfirst'))
         }     
 
         // Check parameter 2 [$encoding]
-        if (!is_string($encoding) && !is_null($encoding)) return $errors[2]; 
-      
-        if (!in_array($encoding, mb_list_encodings(), true)) {
-            static $aliases;
-            if ($aliases === null) {
-                $aliases = [];
-          
-                foreach (mb_list_encodings() as $encoding) {
-                    $encoding_aliases = mb_encoding_aliases($encoding);
-            
-                    if ($encoding_aliases) {
-                        foreach ($encoding_aliases as $alias) {
-                            $aliases[] = $alias;
-                        }
-                    }
-                }
-            }
-        
-            if (!in_array($encoding, $aliases, true)) {
-                trigger_error($errors[3], E_USER_WARNING);
-                return '';
-            }
-        }
+        $encoding = validate_encoding($encoding, 'mb_ucfirst');
+
         /************************
          * END CHECK FOR ERRORS
          ***********************/
@@ -111,6 +89,8 @@ if (!function_exists('mb_ucfirst'))
 /**
  * If the function [ mb_lcfirst ] does not exist then we create it.
  * ++ 8.4.0 ---- https://wiki.php.net/rfc/mb_ucfirst
+ * 
+ * @since 1.0.5
  */
 if (!function_exists('mb_lcfirst'))
 {
@@ -122,18 +102,13 @@ if (!function_exists('mb_lcfirst'))
      */
     function mb_lcfirst($string, $encoding = null)
     {
-        // If $encoding is empty then we take the default internal encoding, otherwise we use the one given by the user.
-        $encoding = (is_null($encoding)) ? $encoding = mb_internal_encoding() : (string) $encoding;
-
         /**
          * 
          * Array with Strings Errors for this function.
          */
         $errors = array (
             'mb_lcfirst(): expects parameter 1 to be string, '.gettype($string).' given',
-            'mb_lcfirst(): Parameter 1 must not be empty',
-            'mb_lcfirst(): expects parameter 2 to be string, '.gettype($encoding).' given',          
-            'mb_lcfirst(): Unknown encoding "'.$encoding.'"'            
+            'mb_lcfirst(): Parameter 1 must not be empty',          
         );
 
         /******************
@@ -151,39 +126,196 @@ if (!function_exists('mb_lcfirst'))
         }     
 
         // Check parameter 2 [$encoding]
-        if (!is_string($encoding) && !is_null($encoding)) return $errors[2]; 
-      
-        if (!in_array($encoding, mb_list_encodings(), true)) {
-            static $aliases;
-            if ($aliases === null) {
-                $aliases = [];
-          
-                foreach (mb_list_encodings() as $encoding) {
-                    $encoding_aliases = mb_encoding_aliases($encoding);
-            
-                    if ($encoding_aliases) {
-                        foreach ($encoding_aliases as $alias) {
-                            $aliases[] = $alias;
-                        }
-                    }
-                }
-            }
-        
-            if (!in_array($encoding, $aliases, true)) {
-                trigger_error($errors[3], E_USER_WARNING);
-                return '';
-            }
-        }
+        $encoding = validate_encoding($encoding, 'mb_lcfirst');
+
         /************************
          * END CHECK FOR ERRORS
          ***********************/
-
-
 
         return mb_strtolower(mb_substr($string, 0, 1, $encoding), $encoding) . mb_substr($string, 1, null, $encoding);
     }
 }
 
+
+
+
+
+/**
+ * If the function [ mb_ltrim ] does not exist then we create it.
+ * ++ 8.4.0 ---- https://php.watch/versions/8.4/mb_trim-mb_ltrim-mb_rtrim
+ * 
+ * @since 1.0.5
+ */
+if (!function_exists('mb_ltrim'))
+{
+    /**  
+    * Multi-byte safely strip white-spaces (or other characters) from the beginning of a string.  
+    *
+    * @param string $string The string that will be trimmed.  
+    * @param string $characters Optionally, the stripped characters can also be specified using the $characters parameter. Simply list all characters that you want to be stripped.  
+    * @param string|null $encoding The encoding parameter is the character encoding.  
+    *
+    * @return string The trimmed string.  
+    */
+    function mb_ltrim($string, $characters = " \f\n\r\t\v\x00\u{00A0}\u{1680}\u{2000}\u{2001}\u{2002}\u{2003}\u{2004}\u{2005}\u{2006}\u{2007}\u{2008}\u{2009}\u{200A}\u{2028}\u{2029}\u{202F}\u{205F}\u{3000}\u{0085}\u{180E}", $encoding = null)
+    {        
+        /**
+         * Array with Strings Errors for this function.
+         */
+        $errors = array (
+            'mb_ltrim(): expects parameter 1 to be string, '.gettype($string).' given',
+            'mb_ltrim(): Parameter 1 must not be empty',
+        );
+
+        /******************
+         * Check for Errors
+         *****************/ 
+        // Check parameter 1 [$string]
+        if (!is_string($string) && !is_scalar($string) && !(is_object($string) && method_exists($string, '__toString'))) {
+            trigger_error($errors[0], E_USER_WARNING);
+            return '';
+        }
+
+        if ($string === '') {
+            trigger_error($errors[1], E_USER_WARNING);
+            return '';
+        }     
+
+        // Check parameter 3 [$encoding]
+        $encoding = validate_encoding($encoding, 'mb_ltrim');
+
+        /************************
+         * END CHECK FOR ERRORS
+         ***********************/
+        
+        $string = mb_convert_encoding($string, $encoding);
+
+        //quote characters for use in a characters list
+        $characters = preg_replace('!([\\\\\\-\\]\\[/^])!','\\\${1}',$characters);       
+        
+        return preg_replace('/^['.$characters.']+/u',"", $string);
+    }
+}
+
+
+
+
+
+/**
+ * If the function [ mb_rtrim ] does not exist then we create it.
+ * ++ 8.4.0 ---- https://php.watch/versions/8.4/mb_trim-mb_ltrim-mb_rtrim
+ * 
+ * @since 1.0.5
+ */
+if (!function_exists('mb_rtrim'))
+{
+    /**  
+     * Multi-byte safely strip white-spaces (or other characters) from the end of a string.  
+     *
+     * @param string $string The string that will be trimmed.  
+     * @param string $characters Optionally, the stripped characters can also be specified using the $characters parameter. Simply list all characters that you want to be stripped.  
+     * @param string|null $encoding The encoding parameter is the character encoding.  
+     *
+     * @return string The trimmed string.  
+     */
+    function mb_rtrim($string, $characters = " \f\n\r\t\v\x00\u{00A0}\u{1680}\u{2000}\u{2001}\u{2002}\u{2003}\u{2004}\u{2005}\u{2006}\u{2007}\u{2008}\u{2009}\u{200A}\u{2028}\u{2029}\u{202F}\u{205F}\u{3000}\u{0085}\u{180E}", $encoding = null)
+    {
+        /**
+         * Array with Strings Errors for this function.
+         */
+        $errors = array (
+            'mb_rtrim(): expects parameter 1 to be string, '.gettype($string).' given',
+            'mb_rtrim(): Parameter 1 must not be empty',
+        );
+
+        /******************
+         * Check for Errors
+         *****************/ 
+        // Check parameter 1 [$string]
+        if (!is_string($string) && !is_scalar($string) && !(is_object($string) && method_exists($string, '__toString'))) {
+            trigger_error($errors[0], E_USER_WARNING);
+            return '';
+        }
+
+        if ($string === '') {
+            trigger_error($errors[1], E_USER_WARNING);
+            return '';
+        }     
+
+        // Check parameter 3 [$encoding]
+        $encoding = validate_encoding($encoding, 'mb_rtrim');
+
+        /************************
+         * END CHECK FOR ERRORS
+         ***********************/        
+        
+        //$string = mb_convert_encoding($string, $encoding);
+        $string = mb_convert_encoding($string, $encoding);
+
+        //quote charlist for use in a characterclass
+        $characters = preg_replace('!([\\\\\\-\\]\\[/^])!','\\\${1}',$characters);
+
+        return preg_replace('/['.$characters.']+$/u','',$string);  
+    }
+}
+
+
+
+/**
+ * If the function [ mb_trim ] does not exist then we create it.
+ * ++ 8.4.0 ---- https://php.watch/versions/8.4/mb_trim-mb_ltrim-mb_rtrim
+ * 
+ * @since 1.0.5
+ */
+if (!function_exists('mb_trim'))
+{
+    /**  
+    * Multi-byte safely strip white-spaces (or other characters) from the beginning and end of a string.  
+    * 
+    * @param string $string The string that will be trimmed.  
+    * @param string $characters Optionally, the stripped characters can also be specified using the $characters parameter. Simply list all characters that you want to be stripped.  
+    * @param string|null $encoding The encoding parameter is the character encoding.  
+    *
+    * @return string The trimmed string.  
+    */
+    
+    function mb_trim($string, $characters = " \f\n\r\t\v\x00\u{00A0}\u{1680}\u{2000}\u{2001}\u{2002}\u{2003}\u{2004}\u{2005}\u{2006}\u{2007}\u{2008}\u{2009}\u{200A}\u{2028}\u{2029}\u{202F}\u{205F}\u{3000}\u{0085}\u{180E}", $encoding = null)
+    {
+        /**
+         * Array with Strings Errors for this function.
+         */
+        $errors = array (
+            'mb_trim(): expects parameter 1 to be string, '.gettype($string).' given',
+            'mb_trim(): Parameter 1 must not be empty',
+        );
+
+        /******************
+         * Check for Errors
+         *****************/ 
+        // Check parameter 1 [$string]
+        if (!is_string($string) && !is_scalar($string) && !(is_object($string) && method_exists($string, '__toString'))) {
+            trigger_error($errors[0], E_USER_WARNING);
+            return '';
+        }
+
+        if ($string === '') {
+            trigger_error($errors[1], E_USER_WARNING);
+            return '';
+        }     
+
+        // Check parameter 2 [$encoding]
+        $encoding = validate_encoding($encoding, 'mb_trim');
+
+        /************************
+         * END CHECK FOR ERRORS
+         ***********************/
+
+        //$string = mb_convert_encoding($string, $encoding);
+        $string = mb_convert_encoding($string, $encoding);
+
+        return mb_ltrim(mb_rtrim($string, $characters, $encoding), $characters, $encoding);        
+    }
+}    
 
 
 ?>
